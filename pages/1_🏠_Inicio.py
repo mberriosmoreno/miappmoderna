81ce4c33ae3d52ff80b7d4cc759048029f0e93b4
mberriosmoreno <94083004+mberriosmoreno@users.noreply.github.com>
2025-01-29 20:00:41 -0600

    Create 1_🏠_Inicio.py

---
+++ pages/1_🏠_Inicio.py
@@ -0,0 +1,5 @@
+# pages/1_🏠_Inicio.py
+import streamlit as st
+
+st.title("Página de Inicio 🏠")
+st.write("Bienvenido a la página de inicio. Aquí puedes encontrar información general sobre la aplicación.")
